56e14a017b0837ecbdd4bb19f17a3eaa298d2b7e
kari <evergreenkary@gmail.com>
2023-02-13 21:35:27 +0800

    move tests to different dir

--- evm-core/src/executor.rs
+++ evm-core/src/executor.rs
@@ -202,109 +202,27 @@ impl ExecutionContext {
                     self.logs.borrow_mut().push(log);
                     self.execution_machine.pc.increment_by(1);
                 }
+
+                OpCode::CALLVALUE => {
+                    let callvalue = self.execution_env.value;
+                    self.execution_machine.stack.push(callvalue)?;
+                    self.execution_machine.pc.increment_by(1);
+                }
+
+                OpCode::RETURN => {
+                    let offset = self.execution_machine.stack.pop()?.as_usize();
+                    let size = self.execution_machine.stack.pop()?.as_usize();
+                    let value = self.execution_machine.memory.read_bytes(offset, size);
+
+                    for i in value.into_iter() {
+                        return_value.push(i);
+                    }
+
+                    return Ok(return_value);
+                }
             }
         }
 
         Ok(return_value)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn simple_arithmetic_operations() {
-        let program = vec![
-            0x60, 0x03, 0x60, 0x03, 0x01, 0x60, 0x03, 0x01, 0x60, 0x1B, 0x04, 0x60, 0x03, 0x02,
-        ];
-        let mut context = ExecutionContext::new(Rc::new(GlobalEnvironment {}));
-
-        assert!(context.run(program).is_ok());
-        assert_eq!(context.execution_machine.stack.height(), 1);
-        assert_eq!(
-            U256::from(0x09),
-            context.execution_machine.stack.pop().unwrap()
-        );
-    }
-
-    #[test]
-    fn swap_operations() {
-        let program = vec![0x60, 0x69, 0x60, 0x33, 0x90];
-        let mut context = ExecutionContext::new(Rc::new(GlobalEnvironment {}));
-
-        assert!(context.run(program).is_ok());
-        assert_eq!(
-            context.execution_machine.stack.get_from_top(0).unwrap(),
-            U256::from(0x69)
-        );
-        assert_eq!(
-            context.execution_machine.stack.get_from_top(1).unwrap(),
-            U256::from(0x33)
-        );
-    }
-
-    #[test]
-    fn dup_operations() {
-        let program = vec![0x60, 0x69, 0x60, 0x33, 0x80];
-        let mut context = ExecutionContext::new(Rc::new(GlobalEnvironment {}));
-
-        assert!(context.run(program).is_ok());
-        assert_eq!(context.execution_machine.stack.height(), 3);
-        assert_eq!(
-            context.execution_machine.stack.get_from_top(0).unwrap(),
-            U256::from(0x33)
-        );
-        assert_eq!(
-            context.execution_machine.stack.get_from_top(1).unwrap(),
-            U256::from(0x33)
-        );
-    }
-
-    #[test]
-    fn push_operations() {
-        let program = vec![
-            0x62, 0x42, 0x00, 0x69, 0x60, 0x33, 0x61, 0x00, 0x23, 0x60, 0x99,
-        ];
-        let mut context = ExecutionContext::new(Rc::new(GlobalEnvironment {}));
-
-        assert!(context.run(program).is_ok());
-        assert_eq!(context.execution_machine.stack.height(), 4);
-        assert_eq!(
-            context.execution_machine.stack.get_from_top(0).unwrap(),
-            U256::from(0x99)
-        );
-        assert_eq!(
-            context.execution_machine.stack.get_from_top(1).unwrap(),
-            U256::from(0x0023)
-        );
-        assert_eq!(
-            context.execution_machine.stack.get_from_top(3).unwrap(),
-            U256::from(0x420069)
-        );
-    }
-
-    #[test]
-    fn memory_operations() {
-        let program = vec![
-            0x62, 0x00, 0x23, 0x44, 0x60, 0x00, 0x52, 0x60, 0x00, 0x51, 0x60, 0x00, 0x51,
-        ];
-        let mut context = ExecutionContext::new(Rc::new(GlobalEnvironment {}));
-
-        assert!(context.run(program).is_ok());
-
-        let value = context.execution_machine.memory.read_slice(0, 32);
-
-        assert!(context.execution_machine.stack.height() == 2);
-        assert!(32 == context.execution_machine.memory.used_capacity());
-        assert_eq!(U256::from_big_endian(&value), U256::from(0x002344));
-        assert_eq!(
-            U256::from(0x002344),
-            context.execution_machine.stack.get_from_top(0).unwrap()
-        );
-        assert_eq!(
-            U256::from(0x002344),
-            context.execution_machine.stack.get_from_top(1).unwrap()
-        );
     }
 }
